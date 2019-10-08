ad517d1b9105c993ceb81134df6e1638395bfbd2
Santino Valenzuela <svalenzuela@spigroup.com>
2019-10-08 16:23:43 -0400

    Added new component ImageScreen.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -15,6 +15,11 @@ const HomeScreen = ({navigation}) => {
         title="Go to List Demo"
         onPress={() => navigation.navigate('List')}
       />
+
+      <Button 
+        title="Go to Images Demo"
+        onPress={() => navigation.navigate('Images')}
+      />
     </View>
   )
 };
---
+++ src/screens/ImageScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const ImageScreen = () => {
+  return (
+    <View>
+      <Text>Hello!</Text>
+    </View>
+  )
+}
+
+const style = StyleSheet.create({
+
+});
+
+export default ImageScreen;
