d625540142c39b73c300f129dc86cd4893908a7a
Santino Valenzuela <svalenzuela@spigroup.com>
2019-10-09 12:47:47 -0400

    Working on imageDetail and imageScreen components.  Used destructure for props

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
 
-const ImageDetail = (props) => {
-  console.log(props);
+// const ImageDetail = (props) => {
+const ImageDetail = ({ imageSource, title, imageScore }) => {
+  //console.log(props);
 
   return (
     <View>
       {/* <Image source={require('../../assets/beach.jpg')} /> */}
-      <Image source={props.imageSource}/>
+      {/* <Image source={props.imageSource}/>
       <Text style={styles.textStyle}>{props.title}</Text>
+      <Text>Image score - {props.imageScore}</Text> */}
+
+      <Image source={imageSource}/>
+      <Text style={styles.textStyle}>{title}</Text>
+      <Text>Image score - {imageScore}</Text>
     </View>
   )
 }
--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -5,14 +5,14 @@ import ImageDetail from '../components/ImageDetail';
 const ImageScreen = () => {
   return (
     <View>
-      <ImageDetail style={style.textStyle} title="Forest" imageSource={require('../../assets/forest.jpg')} />
-      <ImageDetail style={style.textStyle} title="Beach" imageSource={require('../../assets/beach.jpg')} />
-      <ImageDetail style={style.textStyle} title="Mountain" imageSource={require('../../assets/mountain.jpg')} />
+      <ImageDetail style={style.textStyle} title="Forest" imageSource={require('../../assets/forest.jpg')} imageScore={9} />
+      <ImageDetail style={style.textStyle} title="Beach" imageSource={require('../../assets/beach.jpg')} imageScore={7} />
+      <ImageDetail style={style.textStyle} title="Mountain" imageSource={require('../../assets/mountain.jpg')} imageScore={4} />
     </View>
   )
 }
 
-const style = StyleSheet.create({
+const style = StyleSheet.create({ 
   textStyle: {
     color: "#ff0000",
     fontSize: 50
